80afeee592586142c0ef3790f3c51db458f26674
krisharavsj <krisharsv@gmail.com>
2025-05-29 22:47:56 +0530

    Add files via upload

---
+++ checking alphabet.py
@@ -0,0 +1,6 @@
+x = input("Please enter the character you want to check: ")
+
+if x.isalpha():
+    print("Yes, your character is an alphabet.")
+else:
+    print("No, your character is not an alphabet!")
